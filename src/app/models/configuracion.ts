4ab3fe98052bdda39502b6aa8e3e66ef4da8f800
FreezeByte <rosas.ruizlg@gmail.com>
2019-09-01 23:40:37 -0500

    Solo para git

--- src/app/models/configuracion.ts
+++ src/app/models/configuracion.ts
@@ -3,5 +3,6 @@ export var configuracion = {
     color: "red",
     fondo: "blue",
     titulo: "Aplicacion con Angular",
-    descripcion: "Aprendiendo Angular"
+    descripcion: "Aprendiendo Angular",
+    tipo_letra: "Arial"
 };
